dc3f404280f3d7a820e903345b40c5394fe0999c
dhleekaf <dhleekaf@gmail.com>
2021-04-15 01:23:52 +0900

    진선미 예측결과 v-for 표출 기능 적용

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -3,12 +3,12 @@
     <div class="page-header page-header-small">
       <parallax
         class="page-header-image"
-        style="background-image: url('img/bg6.jpg')"
+        style="background-image: url('img/mister_trot.jpg')"
       >
       </parallax>
       <div class="content-center">
         <div class="container">
-          <h1 class="title">This is our great company.</h1>
+          <h1 class="title">내일의 眞 善 美</h1>
         </div>
       </div>
     </div>
@@ -16,87 +16,73 @@
       <div class="container">
         <div class="row">
           <div class="col-md-8 ml-auto mr-auto text-center">
-            <h2 class="title">Who we are?</h2>
-            <h5 class="description">
-              According to the National Oceanic and Atmospheric Administration,
-              Ted, Scambos, NSIDClead scentist, puts the potentially record low
-              maximum sea ice extent tihs year down to low ice extent in the
-              Pacific and a late drop in ice extent in the Barents Sea.
-            </h5>
+            <h4 class="title">
+              예측해보기:
+              <div>
+                <select
+                  v-model="program"
+                  v-on:input="
+                    updatePrediction(
+                      $event.target.value,
+                      $event.target.selectedIndex
+                    )
+                  "
+                >
+                  <option disabled :value="null">프로그램 선택</option>
+                  <option
+                    v-for="(item, index) in items"
+                    :value="item"
+                    :key="index"
+                  >
+                    {{ item }}
+                  </option>
+                </select>
+              </div>
+            </h4>
           </div>
         </div>
         <div class="separator separator-primary"></div>
-        <div class="section-story-overview">
-          <div class="row">
-            <div class="col-md-6">
-              <div
-                class="image-container image-left"
-                style="background-image: url('img/login.jpg')"
-              >
-                <!-- First image on the left side -->
-                <p class="blockquote blockquote-primary">
-                  "Over the span of the satellite record, Arctic sea ice has
-                  been declining significantly, while sea ice in the
-                  Antarctichas increased very slightly"
-                  <br />
-                  <br />
-                  <small>-NOAA</small>
-                </p>
+        <div class="section section-team text-center">
+          <div class="container" v-show="predicted">
+            <h2 class="title">{{ program }} 예측결과</h2>
+            <div class="team">
+              <div class="row">
+                <div
+                  class="col-md-4"
+                  v-for="singer in results[this.selected_idx]"
+                  :key="singer.name"
+                >
+                  <div class="team-player">
+                    <h3>{{ singer.rank }}</h3>
+                    <img
+                      :src="singer.src"
+                      :alt="singer.name"
+                      class="rounded-circle img-fluid img-raised"
+                    />
+                    <h4 class="title">{{ singer.name }}</h4>
+                    <p class="category text-primary">{{ singer.subtitle }}</p>
+                    <p class="description">
+                      You can write here details about one of your team members.
+                      You can give more details about what they do. Feel free to
+                      add some <a href="#">links</a> for people to be able to
+                      follow them outside the site.
+                    </p>
+                  </div>
+                </div>
               </div>
-              <!-- Second image on the left side of the article -->
-              <div
-                class="image-container"
-                style="background-image: url('img/bg3.jpg')"
-              ></div>
-            </div>
-            <div class="col-md-5">
-              <!-- First image on the right side, above the article -->
-              <div
-                class="image-container image-right"
-                style="background-image: url('img/bg1.jpg')"
-              ></div>
-              <h3>
-                So what does the new record for the lowest level of winter ice
-                actually mean
-              </h3>
-              <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
-              </p>
-              <p>
-                For a start, it does not automatically follow that a record
-                amount of ice will melt this summer. More important for
-                determining the size of the annual thaw is the state of the
-                weather as the midnight sun approaches and temperatures rise.
-                But over the more than 30 years of satellite records, scientists
-                have observed a clear pattern of decline, decade-by-decade.
-              </p>
-              <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
-              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
     <div class="section section-team text-center">
-      <div class="container">
-        <h2 class="title">Here is our team</h2>
+      <div class="container" v-show="false">
+        <h2 class="title">후보들?</h2>
         <div class="team">
           <div class="row">
             <div class="col-md-4">
               <div class="team-player">
+                <h3>眞</h3>
                 <img
                   src="img/avatar.jpg"
                   alt="Thumbnail Image"
@@ -110,19 +96,17 @@
                   some <a href="#">links</a> for people to be able to follow
                   them outside the site.
                 </p>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
+                <!-- <a href="#pablo" class="btn btn-primary btn-icon btn-round"
                   ><i class="fab fa-twitter"></i
                 ></a>
                 <a href="#pablo" class="btn btn-primary btn-icon btn-round"
                   ><i class="fab fa-instagram"></i
-                ></a>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-facebook-square"></i
-                ></a>
+                ></a> -->
               </div>
             </div>
             <div class="col-md-4">
               <div class="team-player">
+                <h3>善</h3>
                 <img
                   src="img/ryan.jpg"
                   alt="Thumbnail Image"
@@ -136,16 +120,11 @@
                   some <a href="#">links</a> for people to be able to follow
                   them outside the site.
                 </p>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-twitter"></i
-                ></a>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-linkedin"></i
-                ></a>
               </div>
             </div>
             <div class="col-md-4">
               <div class="team-player">
+                <h3>美</h3>
                 <img
                   src="img/eva.jpg"
                   alt="Thumbnail Image"
@@ -159,15 +138,6 @@
                   some <a href="#">links</a> for people to be able to follow
                   them outside the site.
                 </p>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-google-plus"></i
-                ></a>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-youtube"></i
-                ></a>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-twitter"></i
-                ></a>
               </div>
             </div>
           </div>
@@ -192,7 +162,46 @@ export default {
         email: "",
         message: "",
       },
+      program: null,
+      items: ["내일은 미스트롯2", "트롯 전국체전"],
+      predicted: false,
+      results: [
+        [
+          {
+            rank: "眞",
+            name: "김태연",
+            src: "img/김태연.jpg",
+            subtitle: "2012년 4월생 전북 부안",
+            desc: "",
+          },
+          {
+            rank: "善",
+            name: "홍지윤",
+            src: "img/홍지윤.jpg",
+            subtitle: "1995년 3월생 전남 장흥",
+            desc: "",
+          },
+          {
+            rank: "美",
+            name: "김다현",
+            src: "img/김다현.jpg",
+            subtitle: "2009년 2월생 충북",
+            desc: "",
+          },
+        ],
+        [],
+      ],
+      selected_idx: 1,
     };
+  },
+  // props: ["value", "items", "input_id"],
+  created() {},
+  methods: {
+    updatePrediction: function (value, idx) {
+      // this.$emit("input", value);
+      this.selected_idx = idx - 1;
+      this.predicted = true;
+    },
   },
 };
 </script>
